01ec2fabdcdb2a2dee1b2f1465ba4dcca2efdefe
vantukk2222 <ntu96109@gmail.com>
2022-04-01 19:49:43 +0700

    Update

---
+++ QLKTX/QLKTX/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QLKTX
+{
+	public partial class Form1 : Form
+	{
+		SqlConnection connectAcc;
+		SqlCommand cmdAcc;
+		string str = "@Data Source=DESKTOP-IHJACRP;Initial Catalog=QLKyTucXa;Integrated Security=True";
+		SqlDataAdapter adapter = new SqlDataAdapter();
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
