2d7258ca7503979fa8c6f7a7fc5db0029083bb2f
vantukk2222 <ntu96109@gmail.com>
2022-04-01 22:26:17 +0700

    Database

---
+++ Tables/dbo.Account.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Account] (
+  [loginid] [int] NOT NULL,
+  [account] [varchar](50) NULL,
+  [password] [varchar](50) NULL,
+  CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED ([loginid])
+)
+ON [PRIMARY]
+GO
